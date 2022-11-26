33a226f94b88c9beef0b0e6041d4a152f3efe0c5
Yifang76 <105815046+Yifang76@users.noreply.github.com>
2022-11-26 16:40:46 +0000

    Init

---
+++ ECST.py
@@ -0,0 +1,36 @@
+import random
+
+
+print("Hello. Welcome to ECST, the evolved, superior version of PCPA. If you are here, then it must indicate that you have overcome PCPA. Well done.")
+q = input("Do you want to play an eternal cycle of suffering and torment? ").capitalize() 
+while True:
+    
+    if q == "Yes": 
+
+        rannum = random.randint(1,1000000000000) 
+
+        answer = input("What is the outputted number? ") 
+        answer = int(answer)
+
+        if answer == rannum: 
+
+            print("You got it correct, well done") 
+
+        else: 
+
+            print("Try again") 
+
+    else:
+        print("Too bad, I don't care")
+        
+        while q != "Yes":
+            
+
+            rannum = random.randint(1,1000000000000) 
+
+            answer = input("What is the outputted number? ") 
+            answer = int(answer)
+
+            if answer == rannum: 
+
+                print("You got it correct, well done. Now redo it") 
